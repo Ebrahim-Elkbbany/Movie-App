2d8f4b03bd739ff542a89b018066424a2a207f3c
Abdallah akl <dalla.dd123@yahoo.com>
2026-05-21 21:23:26 +0300

    chore: setup images and icons

---
+++ lib/core/assets/app_assets.dart
@@ -0,0 +1,41 @@
+abstract class AppImages {}
+
+abstract class AppSvgImages {
+  static const String base = 'assets/images/svg/';
+ static const List<String> avatars = [
+    avatar01,
+    avatar02,
+    avatar03,
+    avatar04,
+    avatar05,
+    avatar06,
+    avatar07,
+    avatar08,
+    avatar09,
+  ];
+
+  static const String avatar01 = '${base}avatar1.svg';
+  static const String avatar02 = '${base}avatar2.svg';
+  static const String avatar03 = '${base}avatar3.svg';
+  static const String avatar04 = '${base}avatar4.svg';
+  static const String avatar05 = '${base}avatar5.svg';
+  static const String avatar06 = '${base}avatar6.svg';
+  static const String avatar07 = '${base}avatar7.svg';
+  static const String avatar08 = '${base}avatar8.svg';
+  static const String avatar09 = '${base}avatar9.svg';
+}
+
+abstract class AppIcons {
+  static const String base = 'assets/icons/';
+
+  static const String email = '${base}email.svg';
+  static const String person = '${base}person.svg';
+  static const String user = '${base}user.svg';
+  static const String search = '${base}search.svg';
+  static const String phone = '${base}phone.svg';
+  static const String home = '${base}home.svg';
+  static const String lock = '${base}lock.svg';
+  static const String explore = '${base}explore.svg';
+  static const String items = '${base}items.svg';
+  static const String folder = '${base}folder.svg';
+}
