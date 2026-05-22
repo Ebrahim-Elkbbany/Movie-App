605dc5bda74e72f6935f9ae1c66b4e226f36140b
Abdallah akl <dalla.dd123@yahoo.com>
2026-05-22 15:56:39 +0300

    chore: setup AppConstants

--- lib/core/utils/constants/app_constants.dart
+++ lib/core/utils/constants/app_constants.dart
@@ -2,4 +2,31 @@ abstract class AppConstants {
   static const String whatsappNumber = '201050480007';
   static const String whatsappMsg = 'Hello Enza Team';
   static const String defaultCity = 'Alexandria';
+  static const String onBoarding1Title = 'Find Your Next Favorite Movie Here';
+  static const String onBoarding1Description =
+      'Get access to a huge library of movies to suit all tastes. You will surely like it.';
+
+  static const String onBoarding2Title = 'Discover Movies';
+  static const String onBoarding2Description =
+      'Explore a vast collection of movies in all qualities and genres. Find your next favorite film with ease.';
+
+  static const String onBoarding3Title = 'Explore All Genres';
+  static const String onBoarding3Description =
+      'Discover movies from every genre, in all available qualities. Find something new and exciting to watch every day.';
+  static const String onBoarding4Title = 'Create Watchlists';
+  static const String onBoarding4Description =
+      'Save movies to your watchlist to keep track of what you want to watch next. Enjoy films in various qualities and genres.';
+  static const String onBoarding5Title = 'Rate, Review, and Learn';
+  static const String onBoarding5Description =
+      'Share your thoughts on the movies you\'ve watched. Dive deep into film details and help others discover great movies with your reviews.';
+
+  static const String onBoarding6Title = 'Start Watching Now';
+  static const String exploreNow = 'Explore Now';
+  static const String next = 'Next';
+  static const String back = 'Back';
+  static const String finish = 'Finish';
+
+ 
+
+
 }
--- lib/core/utils/constants/assets_paths.dart
+++
@@ -1,7 +0,0 @@
-abstract class AssetsPaths {
-  // Images
-  static const String logo = 'assets/images/logo.png';
-  
-  // Icons & SVGs
-  static const String userIcon = 'assets/imgs/img/user.svg';
-}
